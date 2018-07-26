2a3e28113214e41768c2c0f3f0a161338576efc4
dengxiang <jack.deng@ubtrobot.com>
2018-07-26 10:06:03 +0800

    初始化项目

---
+++ DXSetting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DXSetting
+//
+//  Created by dengxiang on 2018/7/26.
+//  Copyright © 2018年 DX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DXSetting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DXSetting
+//
+//  Created by dengxiang on 2018/7/26.
+//  Copyright © 2018年 DX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
